b1ca2d8b4529df76d3220c18a85d47cba2c59607
irmen <irmen@08cfa0ad-1c00-4aa0-9110-f4b10de7e716>
2012-05-12 16:45:09 +0000

    timer tick mode should be the default I think

--- mudlib/globals.py
+++ mudlib/globals.py
@@ -27,7 +27,7 @@ class __MudContextProxy(object):
 mud_context = __MudContextProxy()
 
 MUD_MAX_SCORE = 100     # arbitrary
-SERVER_TICK_METHOD = "command"    # 'command' (waits for player entry) or 'timer' (async timer driven)
+SERVER_TICK_METHOD = "timer"    # 'command' (waits for player entry) or 'timer' (async timer driven)
 SERVER_TICK_TIME = 1.0    # time between server ticks (in seconds) (usually 1.0 for 'timer' tick method)
 GAMETIME_TO_REALTIME = 5    # meaning: game time is X times the speed of real time (only used with "timer" tick method)
 GAMETIME_EPOCH = datetime.datetime(2012, 4, 19, 14, 0, 0)    # start date/time of the game clock
