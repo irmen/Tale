3eb801c5e6642c793267a0d4acbea74dc3dd6ebb
irmen <irmen@08cfa0ad-1c00-4aa0-9110-f4b10de7e716>
2012-05-12 22:35:46 +0000

    setup script added

---
+++ setup.py
@@ -0,0 +1,34 @@
+from distutils.core import setup
+
+setup(
+    name='tale',
+    version='0.3',
+    packages=['tale'],
+    # package_dir={'': 'tale'},
+    url='http://irmen.home.xs4all.nl/tale/',
+    license='GPL v3',
+    author='Irmen de Jong',
+    author_email='irmen@razorvine.net',
+    description='Mud, mudlib & interactive fiction framework',
+    long_description="""Tale is a framework for creating mudlibs, muds and/or interactive fiction (text adventures).""",
+    keywords="mud, mudlib, interactive fiction, text adventure",
+    scripts=[],
+    platforms="any",
+    classifiers= [
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.2",
+        "Programming Language :: Python :: 3.3",
+        "Topic :: Communications :: Chat",
+        "Topic :: Games/Entertainment",
+        "Topic :: Games/Entertainment :: Multi-User Dungeons (MUD)"
+    ]
+)
