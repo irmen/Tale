0aadeeb15226e991ff0111c0506dce4826b2dde3
irmen <irmen@08cfa0ad-1c00-4aa0-9110-f4b10de7e716>
2012-12-06 23:33:51 +0000

    unittest

---
+++ tests/test_console_io.py
@@ -0,0 +1,29 @@
+"""
+Unit tests for console I/O adapter
+
+'Tale' mud driver, mudlib and interactive fiction framework
+Copyright by Irmen de Jong (irmen@razorvine.net)
+"""
+from __future__ import print_function, division, unicode_literals
+import unittest
+from tale.io import console_io
+
+
+class TestConsoleIo(unittest.TestCase):
+    def test_basic(self):
+        self.assertTrue(console_io.supports_delayed_output)
+        def print(*lines):
+            pass
+        console_io.print = print
+        console_io.break_pressed(None)
+        console_io.output("line1", "line2")
+        del console_io.print
+    def test_async(self):
+        a = console_io.AsyncInput(None)
+        a.disable()
+        a.enable()
+        a.stop()
+
+
+if __name__ == '__main__':
+    unittest.main()
