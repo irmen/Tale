d29479736203f5698623247b9f4e32e976a11eac
Irmen de Jong <irmen@razorvine.net>
2013-09-10 23:26:02 +0200

    version bump

--- tale/__init__.py
+++ tale/__init__.py
@@ -8,7 +8,7 @@ Copyright by Irmen de Jong (irmen@razorvine.net)
 # this is intentionally left empty; don't put stuff here that other modules
 # might need, it's too easy to cause circular dependencies
 
-__version__ = "1.2"
+__version__ = "1.3"
 
 from . import jythonpatch   # patch some stuff in jython, if needed
 
