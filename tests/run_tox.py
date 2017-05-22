0748c21ec6f08ab1a6977ed6e69e3f45b866b4fe
Irmen de Jong <irmen@razorvine.net>
2017-05-22 23:59:42 +0200

    fix tox chdir

--- tests/run_tox.py
+++ tests/run_tox.py
@@ -6,7 +6,10 @@ import unittest
 def run_tests(args):
     if args:
         if args[0] == "-c":
+            print("CHANGEDIR TO", args[1])
             os.chdir(args[1])
+            print("CURDIR:",os.getcwd())
+            sys.path.insert(0, ".")
     suite = unittest.defaultTestLoader.discover(".")
     test_results = unittest.TextTestRunner(verbosity=1).run(suite)
     return len(test_results.errors) + len(test_results.failures)
