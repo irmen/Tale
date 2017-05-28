5e3648bea0a3c78b0d9e3e3dab15b19df93ab4ef
Irmen de Jong <irmen@razorvine.net>
2017-05-28 21:42:39 +0200

    added some tests for driver split

--- tests/test_driver.py
+++ tests/test_driver.py
@@ -13,7 +13,9 @@ import unittest
 
 import tale.base
 import tale.demo
-import tale.driver as the_driver
+import tale.driver
+import tale.driver_if
+import tale.driver_mud
 import tale.util
 from tale.cmds import cmd, wizcmd, disabled_in_gamemode
 from tale.story import GameMode
@@ -28,6 +30,39 @@ def module_level_func_without_ctx():
     pass
 
 
+class TestDriverCreation(unittest.TestCase):
+    def testBase(self):
+        d = tale.driver.Driver()
+        self.assertEqual({}, d.all_players)
+        self.assertIsNone(d.story)
+        self.assertIsNone(d.zones)
+        self.assertIsNone(d.game_clock)
+        self.assertIsNone(d.resources)
+        self.assertIsNone(d.user_resources)
+
+    def testIF(self):
+        d = tale.driver_if.IFDriver(screen_delay=99, gui=False, web=True, wizard_override=True)
+        self.assertEqual(GameMode.IF, d.game_mode)
+        self.assertEqual(99, d.screen_delay)
+        self.assertTrue(d.wizard_override)
+        self.assertEqual("web", d.io_type)
+        self.assertIsNone(d.story)
+        self.assertIsNone(d.zones)
+        self.assertIsNone(d.game_clock)
+        self.assertIsNone(d.resources)
+        self.assertIsNone(d.user_resources)
+
+    def testMud(self):
+        d = tale.driver_mud.MudDriver(True)
+        self.assertEqual(GameMode.MUD, d.game_mode)
+        self.assertTrue(d.restricted)
+        self.assertIsNone(d.story)
+        self.assertIsNone(d.zones)
+        self.assertIsNone(d.game_clock)
+        self.assertIsNone(d.resources)
+        self.assertIsNone(d.user_resources)
+
+
 class TestDeferreds(unittest.TestCase):
     def testSortable(self):
         t1 = datetime.datetime(1995, 1, 1)
@@ -35,18 +70,18 @@ class TestDeferreds(unittest.TestCase):
         t3 = datetime.datetime(1997, 1, 1)
         t4 = datetime.datetime(1998, 1, 1)
         t5 = datetime.datetime(1999, 1, 1)
-        d1 = the_driver.Deferred(t5, os.getcwd, None, None)
-        d2 = the_driver.Deferred(t2, os.getcwd, None, None)
-        d3 = the_driver.Deferred(t4, os.getcwd, None, None)
-        d4 = the_driver.Deferred(t1, os.getcwd, None, None)
-        d5 = the_driver.Deferred(t3, os.getcwd, None, None)
+        d1 = tale.driver.Deferred(t5, os.getcwd, None, None)
+        d2 = tale.driver.Deferred(t2, os.getcwd, None, None)
+        d3 = tale.driver.Deferred(t4, os.getcwd, None, None)
+        d4 = tale.driver.Deferred(t1, os.getcwd, None, None)
+        d5 = tale.driver.Deferred(t3, os.getcwd, None, None)
         deferreds = sorted([d1, d2, d3, d4, d5])
         dues = [d.due_gametime for d in deferreds]
         self.assertEqual([t1, t2, t3, t4, t5], dues)
 
     def test_numeric_deferreds(self):
         thing = tale.base.Item("thing")
-        driver = the_driver.Driver()
+        driver = tale.driver.Driver()
         now = datetime.datetime.now()
         driver.game_clock = tale.util.GameDateTime(now, 1)
         with self.assertRaises(ValueError):
@@ -58,7 +93,7 @@ class TestDeferreds(unittest.TestCase):
 
     def test_datetime_deferreds(self):
         thing = tale.base.Item("thing")
-        driver = the_driver.Driver()
+        driver = tale.driver.Driver()
         now = datetime.datetime.now()
         driver.game_clock = tale.util.GameDateTime(now, 1)
         due = driver.game_clock.plus_realtime(datetime.timedelta(seconds=3601))
@@ -73,11 +108,11 @@ class TestDeferreds(unittest.TestCase):
         t3 = datetime.datetime(1997, 1, 1)
         t4 = datetime.datetime(1998, 1, 1)
         t5 = datetime.datetime(1999, 1, 1)
-        d1 = the_driver.Deferred(t5, os.getcwd, None, None)
-        d2 = the_driver.Deferred(t2, os.getcwd, None, None)
-        d3 = the_driver.Deferred(t4, os.getcwd, None, None)
-        d4 = the_driver.Deferred(t1, os.getcwd, None, None)
-        d5 = the_driver.Deferred(t3, os.getcwd, None, None)
+        d1 = tale.driver.Deferred(t5, os.getcwd, None, None)
+        d2 = tale.driver.Deferred(t2, os.getcwd, None, None)
+        d3 = tale.driver.Deferred(t4, os.getcwd, None, None)
+        d4 = tale.driver.Deferred(t1, os.getcwd, None, None)
+        d5 = tale.driver.Deferred(t3, os.getcwd, None, None)
         heap = [d1, d2, d3, d4, d5]
         heapq.heapify(heap)
         dues = []
@@ -90,24 +125,24 @@ class TestDeferreds(unittest.TestCase):
             pass
         t = Thing()
         due = datetime.datetime.now()
-        d = the_driver.Deferred(due, t.append, [42], None)
+        d = tale.driver.Deferred(due, t.append, [42], None)
         ctx = tale.util.Context(driver=FakeDriver(), clock=None, config=None, player_connection=None)
         d(ctx=ctx)
         self.assertEqual([42], t.x)
-        d = the_driver.Deferred(due, module_level_func, [], None)
+        d = tale.driver.Deferred(due, module_level_func, [], None)
         d(ctx=ctx)
-        d = the_driver.Deferred(due, module_level_func_without_ctx, [], None)
+        d = tale.driver.Deferred(due, module_level_func_without_ctx, [], None)
         d(ctx=ctx)
         with self.assertRaises(ValueError):
-            the_driver.Deferred(due, scoped_function, [], None)
+            tale.driver.Deferred(due, scoped_function, [], None)
         with self.assertRaises(ValueError):
-            d = the_driver.Deferred(due, lambda a, ctx=None: 1, [42], None)
+            d = tale.driver.Deferred(due, lambda a, ctx=None: 1, [42], None)
 
     def testSerializable(self):
         target = Thing()
-        deferreds = [the_driver.Deferred(datetime.datetime.now(), target.append, [1, 2, 3], {"kwarg": 42}),
-                     the_driver.Deferred(datetime.datetime.now(), os.getcwd, [], None),
-                     the_driver.Deferred(datetime.datetime.now(), module_level_func, [], None)]
+        deferreds = [tale.driver.Deferred(datetime.datetime.now(), target.append, [1, 2, 3], {"kwarg": 42}),
+                     tale.driver.Deferred(datetime.datetime.now(), os.getcwd, [], None),
+                     tale.driver.Deferred(datetime.datetime.now(), module_level_func, [], None)]
         ser = pickle.dumps(deferreds, pickle.HIGHEST_PROTOCOL)
         data = pickle.loads(ser)
         self.assertEqual(deferreds, data)
@@ -116,7 +151,7 @@ class TestDeferreds(unittest.TestCase):
         # test due timings where the gameclock == realtime clock
         game_clock = tale.util.GameDateTime(datetime.datetime(2013, 7, 18, 15, 29, 59, 123))
         due = game_clock.plus_realtime(datetime.timedelta(seconds=60))
-        d = the_driver.Deferred(due, os.getcwd, None, None)
+        d = tale.driver.Deferred(due, os.getcwd, None, None)
         result = d.when_due(game_clock)
         self.assertIsInstance(result, datetime.timedelta)
         self.assertEqual(datetime.timedelta(seconds=60), result)
@@ -132,7 +167,7 @@ class TestDeferreds(unittest.TestCase):
         # test due timings where the gameclock == 10 times realtime clock
         game_clock = tale.util.GameDateTime(datetime.datetime(2013, 7, 18, 15, 29, 59, 123), 10)   # 10 times realtime
         due = game_clock.plus_realtime(datetime.timedelta(seconds=60))      # due in (realtime) 60 seconds (600 gametime seconds)
-        d = the_driver.Deferred(due, os.getcwd, None, None)
+        d = tale.driver.Deferred(due, os.getcwd, None, None)
         result = d.when_due(game_clock)   # not realtime
         self.assertIsInstance(result, datetime.timedelta)
         self.assertEqual(datetime.timedelta(seconds=10 * 60), result)
@@ -146,11 +181,11 @@ class TestDeferreds(unittest.TestCase):
 
     def testTimevalueRanges(self):
         with self.assertRaises(AssertionError):
-            the_driver.Deferred(1, os.getcwd, None, None)
-        the_driver.Deferred(datetime.datetime.now(), os.getcwd, None, None)
+            tale.driver.Deferred(1, os.getcwd, None, None)
+        tale.driver.Deferred(datetime.datetime.now(), os.getcwd, None, None)
         with self.assertRaises(ValueError):
-            the_driver.Deferred(datetime.datetime.now(), os.getcwd, None, None, periodical=(0.09, 0.09))
-        driver = the_driver.Driver()
+            tale.driver.Deferred(datetime.datetime.now(), os.getcwd, None, None, periodical=(0.09, 0.09))
+        driver = tale.driver.Driver()
         driver.game_clock = tale.util.GameDateTime(datetime.datetime.now())
         driver.defer(0.9, os.getcwd)
         driver.defer(1.0, os.getcwd)
@@ -194,7 +229,7 @@ def func4(player, parsed, ctx):
 
 class TestCommands(unittest.TestCase):
     def setUp(self):
-        self.cmds = the_driver.Commands()
+        self.cmds = tale.driver.Commands()
         self.cmds.add("verb1", func1)
         self.cmds.add("verb2", func2)
         self.cmds.add("verb3", func2, "wizard")
